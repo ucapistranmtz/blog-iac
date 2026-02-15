86eaa2a7a1b8822b641fa80a694e991daa2a2acb
Ulises <ucapistranmtz@gmail.com>
2026-02-14 19:07:55 -0600

    Adding static website configuration

--- main.tf
+++ main.tf
@@ -23,10 +23,52 @@ provider "aws" {
 
 resource "aws_s3_bucket" "tf-blog-website-bucket" {
   bucket = var.bucket_name
-
   tags = {
     Name        = var.project_name
     Environment = var.environment
   }
 }
 
+
+resource "aws_s3_bucket_configuration" "blog_config" {
+  bucket = aws_s3_bucket.tf-blog-website-bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+
+  error_document {
+    key = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "blog_public_access" {
+  bucket = aws_s3_bucket.tf-blog-website-bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+
+}
+
+resource "aws_s3_bucket_policy" "public_read_policy" {
+  depends_on = [aws_s3_bucket_public_access_block.blog_public_access]
+  bucket     = aws_s3_bucket.tf-blog-website-bucket.id
+
+  policy = jsondecode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "S3.GetObject"
+        Resource  = "${aws_s3_bucket.tf-blog-website-bucket.arn}"
+      }
+    ]
+  })
+
+
+}

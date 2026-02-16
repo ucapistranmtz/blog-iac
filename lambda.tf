fe647baa012a51b5eaf1284bd6fd5a370a3fe36f
Ulises <ucapistranmtz@gmail.com>
2026-02-16 06:42:19 -0600

    infra: adding aws lambda

---
+++ lambda.tf
@@ -0,0 +1,19 @@
+resource "aws_iam_role" "lambda_exec" {
+  name = "${var.project_name}-lambda-exec-role"
+  assume_role_policy = jsonencode({
+    Version = "2017-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionrole"
+}
